2554c6b8ce7ece5700f348e1e02b89538428cbb1
DESKTOP-USAOD01\KrisJ <krzjurczyk@gmail.com>
2017-11-15 21:20:34 +0100

    Ancient Rome cannot into space

--- TddShop/TddShop.Cli.Tests/Order/DiscountCalculatorTests.cs
+++ TddShop/TddShop.Cli.Tests/Order/DiscountCalculatorTests.cs
@@ -1,9 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Moq;
 using TddShop.Cli.Order;
 using TddShop.Cli.Order.Models;
--- TddShop/TddShop.Cli.Tests/Shipment/AncientRomeShippingServiceTests.cs
+++ TddShop/TddShop.Cli.Tests/Shipment/AncientRomeShippingServiceTests.cs
@@ -1,10 +1,7 @@
 ﻿using Moq;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TddShop.Cli.Order.Models;
 using TddShop.Cli.Shipment;
 
 namespace TddShop.Cli.Tests.Shipment
@@ -20,6 +17,82 @@ namespace TddShop.Cli.Tests.Shipment
         {
             _deliveryService = new Mock<IDeliveryService>();
             _target = new AncientRomeShippingService(_deliveryService.Object);
+        }
+
+        [Test, Sequential]
+        public void ShipOrder_BasicOfRomeSymbols_WereWellConverted(
+            [Values(1, 5, 10, 50, 100, 500, 1000)]int digitalNumber, 
+            [Values("I", "V", "X", "L", "C", "D", "M")]string symbol)
+        {
+            //Arrange
+            var dummyOrderModel = new OrderModel{};
+
+            _deliveryService.Setup(x => x.GenerateShipmentReferenceNumber(It.IsAny<int>())).Returns<int>(x=>digitalNumber);
+
+            //Act
+            _target.ShipOrder(dummyOrderModel);
+
+            //Assert
+            _deliveryService.Verify(x=>x.RequestDelivery(symbol, dummyOrderModel), Times.Once);
+        }
+
+        [Test, Sequential]
+        public void ShipOrder_NumbersFrom1To10_WereWellConverted(
+            [Values(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]int digitalNumber,
+            [Values("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X")]string symbol)
+        {
+            //Arrange
+            var dummyOrderModel = new OrderModel{};
+
+            _deliveryService.Setup(x => x.GenerateShipmentReferenceNumber(It.IsAny<int>())).Returns<int>(x => digitalNumber);
+
+            //Act
+            _target.ShipOrder(dummyOrderModel);
+
+            //Assert
+            _deliveryService.Verify(x => x.RequestDelivery(symbol, dummyOrderModel), Times.Once);
+        }
+
+        [Test, Sequential]
+        public void ShipOrder_FewDifferentNumbers_WereWellConverted(
+            [Values(111, 222, 333, 444, 555, 666, 777, 888, 999)]int digitalNumber,
+            [Values("CXI", "CCXXII", "CCCXXXIII", "CDXLIV", "DLV", "DCLXVI", "DCCLXXVII", "DCCCLXXXVIII", "CMXCIX")]string symbol)
+        {
+            //Arrange
+            var dummyOrderModel = new OrderModel { };
+
+            _deliveryService.Setup(x => x.GenerateShipmentReferenceNumber(It.IsAny<int>())).Returns<int>(x => digitalNumber);
+
+            //Act
+            _target.ShipOrder(dummyOrderModel);
+
+            //Assert
+            _deliveryService.Verify(x => x.RequestDelivery(symbol, dummyOrderModel), Times.Once);
+        }
+
+        [Test]
+        public void ShipOrder_ReferenceNumberEqualZero_ThrowException([Values(0)]int digitalNumber)
+        {
+            //Arrange
+            var dummyOrderModel = new OrderModel { };
+
+            _deliveryService.Setup(x => x.GenerateShipmentReferenceNumber(It.IsAny<int>())).Returns<int>(x => digitalNumber);
+            
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => _target.ShipOrder(dummyOrderModel), "Unlucky! Romas did not expect a zero!");
+        }
+
+        [Test]
+        public void ShipOrder_ReferencesValueUnder1000_ThrowException([Random(1001, 10000, 10)]int digitalNumber)
+        {
+            //Arrange
+            var dummyOrderModel = new OrderModel { };
+
+            //Act
+            _deliveryService.Setup(x => x.GenerateShipmentReferenceNumber(It.IsAny<int>())).Returns<int>(x => digitalNumber);
+
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => _target.ShipOrder(dummyOrderModel), "Too high number (max 1000, no idea why :D)");
         }
     }
 }
